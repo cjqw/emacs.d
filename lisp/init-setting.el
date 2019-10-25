29db7d48b6799342e9cd554ca85d5d816f03d4ea
Wei Qiu <weiqiu@microsoft.com>
2019-10-25 15:52:20 +0800

    maximize the frame on startup

--- lisp/init-setting.el
+++ lisp/init-setting.el
@@ -5,5 +5,15 @@
          "D:\\cmder\\vendor\\git-for-windows\\usr\\bin;"
          (getenv "PATH")))
 
+(defun maximize-frame ()
+  "Maximizes the active frame in Windows"
+  (interactive)
+  ;; Send a `WM_SYSCOMMAND' message to the active frame with the
+  ;; `SC_MAXIMIZE' parameter.
+  (when (eq system-type 'windows-nt)
+    (w32-send-sys-command 61488)))
+
+(add-hook 'window-setup-hook 'maximize-frame t)
+
 (provide 'init-setting)
 
